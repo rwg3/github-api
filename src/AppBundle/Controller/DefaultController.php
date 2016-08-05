626be993563be1be4f3805efb04408ca89d5ffa8
Robert <rwg3@users.noreply.github.com>
2016-08-05 09:30:58 +0100

    Created commit, branches and contributors views. Updated DefaultController to handle requests relating to these views.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,10 +12,45 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
+    {      
+        return $this->render('default/index.html.twig');
+    }
+    /**
+     * @Route("/branches", name="branches")
+     */
+    public function branchesShowAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        $client = new \Github\Client();        
+        $branches = $client->api('repo')->branches('symfony', 'symfony');
+        
+        return $this->render('branches.html.twig', [
+            'branches' => $branches
+        ]);
+    }
+    /**
+     * @Route("/commits", name="commits")
+     */
+    public function commitsShowAction(Request $request)
+    {
+        
+        $client = new \Github\Client();        
+        $commits = $client->api('repo')->commits()->all('symfony', 'symfony', array('sha' => 'master'));
+        
+        return $this->render('commits.html.twig', [
+            'commits' => $commits
+        ]);
+    }
+    /**
+     * @Route("/contributors", name="contributors")
+     */
+    public function contributorsShowAction(Request $request)
+    {
+        $client = new \Github\Client();        
+        $response = $client->getHttpClient()->get('repos/symfony/symfony/contributors');
+        $contributors = \Github\HttpClient\Message\ResponseMediator::getContent($response);
+        
+        return $this->render('contributors.html.twig', [
+            'contributors' => $contributors
         ]);
     }
 }
